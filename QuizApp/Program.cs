189dc526f8ac3b08d743d1b2c52211bfd87e4233
sayed_mamdouh <eldeebsayed22@gmail.com>
2025-06-09 22:01:03 +0300

    first update

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         
+            //Question question = new MultiAnswers();
+            Question question = new BoolQuestion();
+            question.CreateQuestion(2);
+
         }
     }
 }
---
+++ QuizApp/Question.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+
+    class Builder
+    {
+
+        public static Question QuestionBuilder(int type)
+        {
+
+            List<string> answers = new();
+            Console.WriteLine("Enter the Question");
+            string? questionHead = Console.ReadLine();
+            if (type == 4)
+            {
+                Console.WriteLine("Enter Your choices ");
+
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write($"{i + 1}.");
+                    answers.Add(Console.ReadLine());
+                }
+            }
+            else
+            {
+                Console.WriteLine("1.True");
+                Console.WriteLine("2.False");
+                answers.Add("True");
+                answers.Add("False");
+
+            }
+            Console.WriteLine("Enter right choose number");
+            int right = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            if (right < 0 || right >= type) return null;
+
+            Console.WriteLine("Enter The degree of Question");
+            double degree = Convert.ToDouble(Console.ReadLine());
+
+            Question question = new MultiAnswers()
+            {
+                QuestionHead = questionHead,
+                Answers = answers,
+                IndexRightAnswer = right,
+                Degree = degree
+            };
+
+            return question;
+
+        }
+    }
+    abstract internal class Question
+    {
+        public string QuestionHead { get; set; }
+        public List<string> Answers { get; set; }
+        public int IndexRightAnswer { get; set; }
+        public double Degree { get; set; }
+
+        public abstract Question CreateQuestion(int type);
+    }
+
+    class MultiAnswers : Question
+    {
+
+        public override Question? CreateQuestion(int type = 4)
+        {
+            return Builder.QuestionBuilder(type);
+        }
+    }
+
+    class BoolQuestion : Question
+    {
+
+        public override Question? CreateQuestion(int type = 2)
+        {
+            return Builder.QuestionBuilder(type);
+
+        }
+
+    }
+
+}
